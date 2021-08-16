28be60a1d8995a05484b25c12afc60f2c65f5461
lurbano <lurbano@earthsciweb.org>
2021-08-16 00:36:28 -0500

    fix

--- webServer/soundTest.py
+++ webServer/soundTest.py
@@ -1,7 +1,7 @@
 import asyncio
 import subprocess
 
-alarm = {h: 0, m: 37, s: 0}
+alarm = {h: 0, m: 38, s: 0}
 
 async def tick(a):
     while 1:
@@ -11,7 +11,7 @@ async def tick(a):
         s = now.tm_sec
         await asyncio.sleep(0.5)
         print(f'{h}:{m}:{s}')
-        if (h == a["h"]) and (m == a["m"]) and (s = a["s"]):
+        if (h == a["h"]) and (m == a["m"]) and (s == a["s"]):
             print("Ring...")
             return "alarm"
 
