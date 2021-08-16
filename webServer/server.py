27487b69d0ef54282af04a1e095f4421571ca070
lurbano <lurbano@earthsciweb.org>
2021-08-15 23:06:53 -0500

    test

--- webServer/server.py
+++ webServer/server.py
@@ -68,7 +68,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 	def open(self):
 		print ('[WS] Connection was opened.')
 		self.write_message('{"who": "server", "info": "on"}')
-		subprocess.Popen('omxplayer -o local 1.mp4')
+		subprocess.Popen('omxplayer -o local 1.mp4', shell=True)
 		#self.oled = oledU(128,32)
 
 
